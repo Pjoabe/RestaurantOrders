abd65e146da51caaf97be728f9678eb1bb52bd96
joabe <joabe9999pereira@gmail.com>
2023-06-25 23:40:13 -0300

    feat- requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,14 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    farinha_trigo = Ingredient("farinha")
+    farinha = Ingredient("farinha")
+    camarao = Ingredient("camarão")
+    assert hash(farinha_trigo) == hash(farinha)
+    assert hash(farinha_trigo) != hash(camarao)
+    assert camarao == camarao
+    assert farinha_trigo != camarao
+    assert repr(farinha_trigo) == "Ingredient('farinha')"
+    assert camarao.name == "camarão"
+    assert farinha_trigo.restrictions == {Restriction.GLUTEN}
