939002aa2e240b28e0569a36f57879011b6f27bc
joabe <joabe9999pereira@gmail.com>
2023-06-26 17:33:47 -0300

    feat- requisito 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,20 @@
+import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
 # Req 2
 def test_dish():
-    pass
+    manteiga = Dish("manteiga", 22.40)
+    rice = Dish("arroz", 15.50)
+    arroz = Ingredient('arroz')
+    with pytest.raises(ValueError):
+        Dish('Camarão', -1035)
+    assert hash(manteiga) != hash(rice)
+    assert hash(rice) == hash(rice)
+    assert manteiga.name == 'manteiga'
+    assert rice == rice
+    manteiga.add_ingredient_dependency(arroz, 15)
+    assert arroz in manteiga.get_ingredients()
+    assert manteiga.get_restrictions() == set()
+    assert manteiga.__repr__() == "Dish('manteiga', R$22.40)"
